242427460d996f6eb467c48f5c7cc0027524bcb5
LokotamaTheMastermind <lokotamathemastermind2@gmail.com>
2020-12-31 14:54:31 +0100

    Added more scripts for windows and mac

---
+++ scripts/mac/install_pip.sh
@@ -0,0 +1,4 @@
+# You can run this in a virtualenv or not
+
+cd ../../ && pip3 install -r requirements_pip.txt
+cd scripts/windows && setup.sh
---
+++ scripts/windows/install_pip.bat
@@ -0,0 +1,4 @@
+@REM You can run this in a virtualenv or not
+
+cd ../../ && pip install -r requirements_pip.txt
+cd scripts/windows && setup.bat
