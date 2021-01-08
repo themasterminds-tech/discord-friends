cbcf53e47be2bbcb1036d9059d704a0c3e4ca89e
LokotamaTheMastermind <lokotamathemastermind2@gmail.com>
2021-01-08 04:28:02 +0100

    Edited background image

--- discord_friends/settings.py
+++ discord_friends/settings.py
@@ -67,23 +67,23 @@ WSGI_APPLICATION = 'discord_friends.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'discord-friends',
-#         'USER': 'postgres',
-#         'PASSWORD': 'admin',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5433',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'discord-friends',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
+    }
+}
 
 
 # Password validation
