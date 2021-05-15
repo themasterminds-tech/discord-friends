6c64656e75bcc7b0c819a5310ed0c32415b35e56
LokotamaTheMastermind <57059138+LokotamaTheMastermind@users.noreply.github.com>
2021-05-15 22:28:46 +0100

    Changed the route of the admin dashboard

--- discord_friends/urls.py
+++ discord_friends/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 from .views import _logout, _login, _register
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('backend/admin/', admin.site.urls),
     path('logout/', _logout, name='logout'),
     path('register/', _register, name='register'),
     path('login/', _login, name='login'),
